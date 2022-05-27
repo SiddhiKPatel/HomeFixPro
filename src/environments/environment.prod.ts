5e75c57ff9ede0888ef1b6b2ef0cf26c69ee2561
SiddhiKPatel <104893208+SiddhiKPatel@users.noreply.github.com>
2022-05-27 09:03:38 +0530

    api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // API: 'http://creativeonlinezone.com/project/homefix'
-  API: 'https://admin.homefixpros.com'
+  // API: 'https://admin.homefixpros.com',
+  API: 'http://20.237.248.105/homefix-laravel'
 };
