947883d8f2f8c98980d8cfbb3b9d8dfe1b96384e
SiddhiKPatel <104893208+SiddhiKPatel@users.noreply.github.com>
2022-06-15 19:44:47 +0530

    availability form

--- src/app/pages/availability/availability.component.ts
+++ src/app/pages/availability/availability.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./availability.component.css']
 })
 export class AvailabilityComponent implements OnInit {
-
+  week = [{
+    name: 'Sun',
+    checked: true
+  }, {
+    name: 'Mon',
+    checked: true
+  }, {
+    name: 'Tues',
+    checked: true
+  }, {
+    name: 'Wed',
+    checked: true
+  },
+  {
+    name: 'Thur',
+    checked: true
+  }, {
+    name: 'Fri',
+    checked: true
+  }, {
+    name: 'Sat',
+    checked: true
+  },];
+  time: string[] = ['12 am', '1 am', '2 am', '3 am', '4 am', '5 am', '6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm',
+    '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm', '9 pm', '10 pm', '11 pm']
   constructor() { }
 
   ngOnInit(): void {
   }
+  saveAvailability() {
 
+  }
+  changeCheckbox(tags) {
+    if (tags) {
+
+    }
+  }
 }
